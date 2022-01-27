ae3bf885c5b7d5af87a8bd8f80e8ef694744fc45
polo <lg550055@hotmail.com>
2022-01-26 23:52:00 -0600

    Adds create and delete, and carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,22 @@
-import axios from 'axios';
 import React from 'react';
-import { Button, Card, Carousel, Form } from 'react-bootstrap';
+import axios from 'axios';
+import Pic from './stars.jpeg'
+import BookFormModal from './BookFormModal'
+import { Button, Container, Carousel } from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      showBookForm: false
     }
   }
-
-  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+  
+  url = `${process.env.REACT_APP_SERVER}/books`;
+  /* DONE: Make a GET request to your API to fetch books for the logged in user  */
   getBooks = async () => {
-    let url = `${process.env.REACT_APP_SERVER}/books`;
-    let booksData = await axios.get(url);
+    let booksData = await axios.get(this.url);
     this.setState({books: booksData.data});
   }
 
@@ -22,62 +25,55 @@ class BestBooks extends React.Component {
   }
 
   addBook = async (book) => {
-    let url = `${process.env.REACT_APP_SERVER}/books`;
-    let result = await axios.post(url, book);
-    console.log(result.data);
+    try {
+      let addedBook = await axios.post(this.url, book);
+      this.setState({ books: [...this.state.books, addedBook.data]});
+    } catch(err) {
+      console.error(err)
+    }
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let newBook = {
-      title: e.target.title.value,
-      description: e.target.description.value,
-      status: e.target.status.value,
-      email: this.props.user.email
-    }
-    this.addBook(newBook);
+  deleteBook = async (id) => {
+    try {
+      await axios.delete(this.url + '/' + id);
+      // need to update state, for updated list to render w/o making a new API call
+    } catch(err) { console.error(err) }
   }
+
+  handleClose = () => { this.setState({showBookForm: false})}
 
   render() {
-    /* TODO: render user's books in a Carousel */
+    /* DONE: render user's books in a Carousel */
     let bookList = this.state.books.map((b,i) => 
-      <Card key={i}>
-        <Card.Body>
-          <Card.Title>{b.title}</Card.Title>
-          <Card.Text>{b.description}</Card.Text>
-        </Card.Body>
-      </Card>
-      // <Carousel.Item key={i}>
-      //    <img src='./goodhacker.png' alt=''/>
-      //    <Carousel.Caption>
-      //     <h5>{b.title}</h5>
-      //     <p>{b.description}</p>
-      //    </Carousel.Caption>
-      //  </Carousel.Item>
+      <Carousel.Item key={i}>
+         <img className="d-block w-100" src={Pic} alt='Slide'/>
+         <Carousel.Caption>
+          <h5>{b.title}</h5>
+          <p>{b.description}</p>
+          <Button variant="outline-light" size="sm" onClick={() => this.deleteBook(b._id)}>Delete Book</Button>
+         </Carousel.Caption>
+       </Carousel.Item>
     )
 
     return (
       <>
-        <h4>My Essential Lifelong Learning &amp; Formation Shelf</h4>
-
+        <Container>
+          <h4>My Essential Lifelong Learning &amp; Formation Shelf</h4>
+          <Button variant="outline-dark" size="sm" onClick={() => this.setState({showBookForm: true})}>Add new book</Button>
+        </Container>
+        <BookFormModal
+          show={this.state.showBookForm}
+          handleClose={this.handleClose}
+          addBook={this.addBook}
+          email={this.props.user.email}/>
+        
         {this.state.books.length ?
-          <>{bookList}</>
-          // <Carousel style={{height: '200px'}}>{bookList}</Carousel>
+          <Container>
+            <Carousel >{bookList}</Carousel>
+          </Container>
           : <h3>No Books Found :(</h3>
         }
-
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group controlId='title'>
-            <Form.Control type="text" placeholder="Title" />
-          </Form.Group>
-          <Form.Group controlId='description'>
-            <Form.Control type="text" placeholder="Description" />
-          </Form.Group>
-          <Form.Group controlId='status'>
-            <Form.Control type="text" placeholder="Status" />
-          </Form.Group>
-          <Button type='submit'>Add book</Button>
-        </Form>
+        <br></br>
       </>
     )
   }
